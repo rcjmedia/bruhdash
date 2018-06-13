70475d7ba0e998d3e20bbb9f5319ebd77d91544a
Romeo <romeocorpuz808@gmail.com>
2018-06-12 21:53:44 -1000

    Submit

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,47 +10,107 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
+  first: function (arr) {
+    var elemOne = arr.shift();
+    return elemOne;
       
   },
 
   // returns the last element of an array
-  last: function () {
+  last: function (arr) {
+    var elemLast = arr.pop();
+    return elemLast;
 
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr, elem) {
+    for(var i=0; i<arr.length; i++){
+      if(elem === arr[i]){
+        return i;
+      }
+    }
+    return -1;
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (arr, elem) {
+    for(var i=(arr.length-1); i>=0; i--){
+      if(elem === arr[i]){
+        return i;
+      }
+    }
+    return -1;
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
+  initial: function (arr) {
+    var allLast = [];
+    for(var i=0; i<(arr.length-1); i++){
+      allLast.push(arr[i]);
+    }
+    return allLast;
 
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
+  compact: function(arr) {
+    var notFalseArray = [];
+
+    for(var i=0; i<arr.length; i++){
+      var elem = arr[i];
+
+      if(elem !== false && elem !== null && elem !== 0 && elem !== "" && elem!== undefined && !isNaN(elem)){
+        notFalseArray.push(elem);
+      }
+    }
+    return notFalseArray;
 
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr, start, end) {
+    var arraySliced = [];
+    for(var i=start; i<end; i++){
+      arraySliced.push(arr[i]);
+    }
+    return arraySliced;
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
+  drop: function(arr, num){
+    if(num === undefined){
+      arr.shift();
+      return arr;
+    }
+    else if(num === 0){
+      return arr;
+    }
+    else{
+      while(num>0){
+        arr.shift();
+        num--;
+      }
+      return arr;
+    }
   },
 
   // returns a slice of array with n elements dropped from the end
-  dropRight: function() {
+  dropRight: function(arr, num) {
+    if(num === undefined){
+      arr.pop();
+      return arr;
+    }
+    else if(num === 0){
+      return arr;
+    }
+    else{
+      while(num>0){
+        arr.pop();
+        num--;
+      }
+      return arr;
+    }
 
   },
 
